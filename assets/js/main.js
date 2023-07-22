1455b3c197b843b57521f7010ab3229634e3bdb3
cindyeme <emereninicynthia@gmail.com>
2023-07-22 18:08:23 +0100

    completed service modals

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
+const modals = {
+  1: {
+    overlay: document.getElementById("modalOverlay1"),
+    modal: document.getElementById("modal1"),
+  },
+  2: {
+    overlay: document.getElementById("modalOverlay2"),
+    modal: document.getElementById("modal2"),
+  },
+};
 
-const progress = document.querySelector(".progress-line");
+function openModal(modalNumber) {
+  const modalData = modals[modalNumber];
+  modalData.overlay.classList.add("show-modal");
+  modalData.overlay.classList.remove("hide-modal");
+}
 
-// progress.style.width = progress.getAttribute("data-done") + "%";
-// progress.style.opacity = 1;
+function closeModal(modalNumber) {
+  const modalData = modals[modalNumber];
+  modalData.overlay.classList.add("hide-modal");
+  modalData.overlay.classList.remove("show-modal");
+}
